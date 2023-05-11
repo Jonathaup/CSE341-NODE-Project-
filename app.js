c1e2cf23a560311e0007c69e16b652b21993c05d
Jonathaup <Jonathanjacoburibepinto@gmail.com>
2023-05-11 10:28:37 -0600

    Lesson 7 and 8

--- app.js
+++ app.js
@@ -21,13 +21,12 @@ app
   .use(passport.initialize())
   .use(passport.session())
   .use((req, res, next) => {
-    res.setHeader('Access-Controll-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-Width, Content-Type, Accept, Z-Key, Authorization'
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
     );
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATH, OPTIONS, DELETE');
-    res.setHeader("Content-Security-Policy", "default-src 'none'; img-src 'self' data: https:;");
     next();
   })
 
