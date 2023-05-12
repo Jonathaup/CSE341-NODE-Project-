86223fdc3bfa1a3929af55bc4735e6d176e42c7a
Jonathan Uribe <103954802+Jonathaup@users.noreply.github.com>
2023-05-12 08:41:01 -0600

    Lesson 8

--- server.js
+++ server.js
@@ -1,16 +1,77 @@
 const express = require('express');
-const app = express();
-const mongodb = require('./db/connect');
-const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+const session = require('express-session');
+const passport = require('passport');
+const port = process.env.PORT || 3000;
+const app = express();
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 
 app
   .use(bodyParser.json())
+  .use(
+    session({
+      secret: 'mysecret',
+      resave: false,
+      saveUninitialized: true
+    })
+  )
+
+  .use(passport.initialize())
+  .use(passport.session())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATH, OPTIONS, DELETE');
+    res.setHeader("Content-Security-Policy", "default-src 'none'; img-src 'self' data: http://localhost:3000; style-src-elem 'self' 'unsafe-inline'; script-src-elem 'self' http://localhost:3000; connect-src 'self' http://localhost:3000;");
     next();
   })
-  .use('/', require('./routes'));
+
+  .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+  .use(cors({ origin: '*' }))
+  .use('/', require('./routes/index.js'));
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get('/', (req, res) => {
+  res.send(
+    req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged out'
+  );
+});
+
+app.get(
+  '/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/api-docs',
+    session: false
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  }
+);
 
 mongodb.initDb((err) => {
   if (err) {
@@ -20,3 +81,7 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+
+// req.isAuthenticated is provided from the auth router
